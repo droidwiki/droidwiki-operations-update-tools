65c2cf1ee39d02e5f09436866267940a2c5b7f0b
Florian <florian.schmidt.welzow@t-online.de>
2017-12-22 23:41:53 +0100

    Change how updates are done for extensions
    Instead of having a copied repository for each extension, which mostly
    just holds the same code as the "official" one, the extension will now be
    updated from a second remote, which is the "official" one, and then patches
    will be applied if needed. This reduces the overhead of updating extensions
    and minimizes the update to just fetching the remote, checking out the new
    version branch (wmf/...) and applying patches, if available.
    
    This will only be done for extensions listed in the extensions.list and skins
    in the skins.list. Extensions, which are not developed by the WMF, will still
    need to be updated manually with git pull e.g.

--- update.sh
+++ update.sh
@@ -8,10 +8,8 @@
 #  -s Submodules should submodules updated, too?
 #  -d Debug Output all the spam!
 
-branchPath="ssh://florianschmidtwelzow@gerrit.wikimedia.org:29418/mediawiki/extensions/"
+branchPath="https://gerrit.wikimedia.org/r/mediawiki/extensions/"
 wmf="wmf"
-keepignore="false"
-verbose="false"
 
 while getopts ":u:v:p:w:skd" opt; do
   case $opt in
@@ -27,12 +25,6 @@ while getopts ":u:v:p:w:skd" opt; do
     w)
       wmf=$OPTARG
       ;;
-    k)
-      keepignore="true"
-      ;;
-    d)
-      debug="true"
-      ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       ;;
@@ -40,73 +32,69 @@ while getopts ":u:v:p:w:skd" opt; do
 done
 
 function coloredEcho(){
-  if [ $verbose = true ] ; then
-    local exp=$1;
-    local color=$2;
-    if ! [[ $color =~ '^[0-9]$' ]] ; then
-      case $(echo $color | tr '[:upper:]' '[:lower:]') in
-       black) color=0 ;;
-       red) color=1 ;;
-       green) color=2 ;;
-       yellow) color=3 ;;
-       blue) color=4 ;;
-       magenta) color=5 ;;
-       cyan) color=6 ;;
-       white|*) color=7 ;; # white or invalid color
-      esac
-    fi
-    tput setaf $color;
-    echo $exp;
-    tput sgr0;
+  local exp=$1;
+  local color=$2;
+  if ! [[ $color =~ '^[0-9]$' ]] ; then
+    case $(echo $color | tr '[:upper:]' '[:lower:]') in
+     black) color=0 ;;
+     red) color=1 ;;
+     green) color=2 ;;
+     yellow) color=3 ;;
+     blue) color=4 ;;
+     magenta) color=5 ;;
+     cyan) color=6 ;;
+     white|*) color=7 ;; # white or invalid color
+    esac
   fi
+  tput setaf $color;
+  echo $exp;
+  tput sgr0;
 }
-echo "Starting update of $project to $wmf/$newBranch using $branchPath."
 
-shopt -s dotglob
+coloredEcho "Starting update of $project to $wmf/$newBranch using $branchPath." yellow
 
-coloredEcho "cd to $project" green
+shopt -s nullglob
+IFS='%'
+
+if [ ! -d "$project" ]; then
+  coloredEcho "The directory $project does not exist" red
+  exit;
+fi
+
+coloredEcho " | cd to $project..." green
 cd $project/
 
-coloredEcho "Prepare git repository" green
-git checkout master
-git pull origin master
-coloredEcho "create working dirs" green
-mkdir {new,old}
-cd new
-
-coloredEcho "clone the latest revision" green
-git clone -b $wmf/$newBranch --single-branch --depth 1 --recursive $branchPath$project .
-coloredEcho "fetch new branch" green
-git fetch
-git checkout $wmf/$newBranch
-
-coloredEcho "Get version information" green
-sha1=`cat .git/refs/heads/wmf/$newBranch`
-
-[[ "$sha1" = "" ]] && coloredEcho "$newBranch seems to be not a valid branch in wikimedias git. Exit." && exit 1;
-
-coloredEcho "delete unnecessary files and folders" green
-rm -rf .git .gitreview
-if [ $keepignore = true ] ; then
-  rm -rf .gitignore
+coloredEcho " | Prepare git repository..." green
+git reset --hard &> /dev/null
+git clean -f -d &> /dev/null
+git ls-remote --exit-code upstream &> /dev/null
+if test $? != 0; then
+    git remote add upstream $branchPath$project
 fi
-cd ..
 
-coloredEcho "Move new revision" green
-mv * old/
-mv old/.git ./
-mv old/.gitreview ./
-if [ $keepignore = true ] ; then
-  mv old/.gitignore ./
+coloredEcho " | Fetch upstream..." green
+git fetch upstream &> /dev/null
+if [ `git ls-remote upstream $wmf/$newBranch | wc -l` != 1 ]; then
+  coloredEcho "The target branch $wmf/$newBranch does not exist in the remote..." red
+  exit;
 fi
-mv old/new/* ./
-rm -rf old
 
-coloredEcho "Add to git and commit" green
-git add --all
-git commit -m "Update $project to $newBranch" -m "Forward to $sha1"
+git rev-parse --verify $wmf/$newBranch &> /dev/null
+if [ $? == 0 ]; then
+  coloredEcho " | Deleting already existing local branch $wmf/$newBranch" yellow
+  git checkout master &> /dev/null
+  git branch -D $wmf/$newBranch > /dev/null
+fi
 
-coloredEcho "Commit to review" green
-git push origin master
+git checkout -b $wmf/$newBranch upstream/$wmf/$newBranch &> /dev/null
+git pull &> /dev/null
 
-coloredEcho "Finished." green
+coloredEcho " | Applying patches, if available..." green
+for patch in $(dirname $0)/patches/$project/*.patch ; do
+  coloredEcho " | Applying patch $patch..." green
+  git am $patch
+done
+
+git submodule update --init &> /dev/null
+
+coloredEcho "Finished" yellow
