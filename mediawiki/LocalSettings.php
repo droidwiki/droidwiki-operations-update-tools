2af5e765c1b76359c3a1076a6d46c6ee6ae88af1
Florian <florian.schmidt.welzow@t-online.de>
2019-11-17 05:05:36 +0100

    Include wikibase messages in l10n update of pipeline

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -6,4 +6,10 @@
 require_once "$IP/../mw-config/mw-config/multiversion/MWMultiVersion.php";
 $multiversion = MWMultiVersion::getInstance();
 $wgServer = "https://www.droidwiki.org";
+
+# Wikibase is kind of a special case here as it is loaded conditionally where the default is
+# "do not load". This breaks the l10n update, as WB measges are not included.
+$wmgUseWikibaseRepo = true;
+$wmgUseWikibaseClient = true;
+
 require_once "$IP/../mw-config/mw-config/ExtensionSetup.php";
