66cd7fb2ae3b359fdf8c8a5dd346d3d3ab044e32
Florian <florian.schmidt.welzow@t-online.de>
2020-11-21 16:46:06 +0100

    Add first log group for nginx containers

--- aws/lib/docker-logs-stack.ts
+++ aws/lib/docker-logs-stack.ts
@@ -1,11 +1,18 @@
 import * as cdk from '@aws-cdk/core';
+import {RemovalPolicy} from '@aws-cdk/core';
 import {Effect, Group, ManagedPolicy, PolicyStatement, User} from "@aws-cdk/aws-iam";
+import {LogGroup, RetentionDays} from "@aws-cdk/aws-logs";
 
 export class DockerLogsStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         this.iamPermissions();
+        new LogGroup(this, 'frontend-proxy', {
+            logGroupName: '/docker/frontend-proxy',
+            removalPolicy: RemovalPolicy.DESTROY,
+            retention: RetentionDays.ONE_WEEK
+        });
     }
 
     private iamPermissions() {
@@ -21,16 +28,22 @@ export class DockerLogsStack extends cdk.Stack {
     }
 
     private dockerLogsPolicy() {
-        const dockerLogsPolicy = new PolicyStatement({
+        const createLogStream = new PolicyStatement({
             effect: Effect.ALLOW,
             actions: [
                 'logs:CreateLogStream',
+            ],
+            resources: ['arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:log-group:/docker/*']
+        });
+        const putLogEvent = new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
                 'logs:PutLogEvents',
             ],
-            resources: ['*']
+            resources: ['arn:aws:logs:${AWS::Region}:${AWS::AccountId}:log-group:log-group:/docker/*:log-stream:*']
         });
         return new ManagedPolicy(this, 'docker-awslogs', {
-            statements: [dockerLogsPolicy],
+            statements: [createLogStream, putLogEvent],
         });
     }
 }
